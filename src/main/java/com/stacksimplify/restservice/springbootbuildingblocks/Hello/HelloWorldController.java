5fdcb0b0a825fbdf0bf0c414ad1cd78095260950
rashmi-agrawal-0602 <r.agrawal0602@gmail.com>
2019-12-03 16:00:35 +0530

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservice/springbootbuildingblocks/Hello/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.stacksimplify.restservice.springbootbuildingblocks.Hello;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stacksimplify.restservice.springbootbuildingblocks.Entity.UserDetails;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+	
+	//@RequestMapping(method = RequestMethod.GET , path = "/helloworld")
+	@GetMapping("/helloworld1")
+	public String heLloWorld() {
+		return "Hello World1";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("abc","xyz","blr");
+	}
+}
