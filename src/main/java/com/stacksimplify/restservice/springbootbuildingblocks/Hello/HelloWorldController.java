317e85fc1dd80e62e6f78c38a8e96d8132f89ec6
rashmi-agrawal-0602 <r.agrawal0602@gmail.com>
2019-12-03 16:15:35 +0530

    Hello World Basee

--- src/main/java/com/stacksimplify/restservice/springbootbuildingblocks/Hello/HelloWorldController.java
+++ src/main/java/com/stacksimplify/restservice/springbootbuildingblocks/Hello/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.stacksimplify.restservice.springbootbuildingblocks.Entity.UserDetails;
 
+//Rest Controller class
 @RestController
 @RequestMapping("/api")
 public class HelloWorldController {
